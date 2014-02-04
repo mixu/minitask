6e41987757c81906f6b6a01e4e0d6af52927da46
Mikito Takada <mikito.takada@gmail.com>
2014-02-03 19:04:53 -0800

    Add more tests

--- test/cache.test.js
+++ test/cache.test.js
@@ -2,17 +2,19 @@ var fs = require('fs'),
     assert = require('assert'),
     Cache = require('minitask').Cache;
 
+var cachePath = __dirname + '/tmp';
+
 exports['cache tests'] = {
 
   'instance() returns the same instance for the same path/method combo': function() {
     assert.strictEqual(
-      Cache.instance({ path: __dirname + '/tmp', method: 'stat' }),
+      Cache.instance({ path: cachePath, method: 'stat' }),
       Cache.instance({ path: __dirname + '/./tmp/../tmp/', method: 'stat' })
     );
   },
 
   'can store metadata at the root': function() {
-    var cache = Cache.instance({ path: __dirname + '/tmp', method: 'stat' }),
+    var cache = Cache.instance({ path: cachePath, method: 'stat' }),
         key = Cache.hash(JSON.stringify({ test: new Date() })),
         value = cache.data(key);
     // get
@@ -23,7 +25,7 @@ exports['cache tests'] = {
   },
 
   'can store metadata about a file': function() {
-    var cache = Cache.instance({ path: __dirname + '/tmp', method: 'stat' }),
+    var cache = Cache.instance({ path: cachePath, method: 'stat' }),
         key = 'dependencies',
         value = cache.file(__dirname+'/fixtures/bar.txt').data(key);
     // get
@@ -33,11 +35,8 @@ exports['cache tests'] = {
     assert.deepEqual(cache.data(key), { abc: 'def' });
   },
 
-
-
   'can store the result of a computation': function(done) {
-    var cachePath = __dirname + '/tmp',
-        inputFilePath = __dirname+'/fixtures/bar.txt',
+    var inputFilePath = __dirname+'/fixtures/bar.txt',
         cache = Cache.instance({ path: cachePath, method: 'stat' }),
         taskHash = Cache.hash(JSON.stringify({ test: new Date() })),
         cacheFilePath = cache.file(inputFilePath).path(taskHash);
@@ -59,66 +58,58 @@ exports['cache tests'] = {
   },
 
   'using stat, when the underlying file changes, the stored items are invalidated': function() {
-    // should emit "outdated"
+    var cache = Cache.instance({ path: cachePath, method: 'stat' }),
+        taskHash = 'test3',
+        trackFile = __dirname+'/fixtures/hash.txt';
+
+    fs.writeFileSync(trackFile, 'foo');
+
+    // create the file in the cache folder
+    cacheFile = cache.filepath();
+    fs.writeFileSync(trackFile, '123');
+    // mark as complete
+    cache.file(trackFile).path(taskHash, cacheFile);
+    assert.equal(cache.file(trackFile).path(taskHash), cacheFile);
+
+    // 1) when the task hash is changed, the lookup is invalidated
+    taskHash = 'test4';
+    assert.ok(!cache.file(trackFile).path(taskHash));
+
+    // 2) when the file changes => invalidate
+    taskHash = 'test3';
+    fs.writeFileSync(trackFile, 'foobar');
+
+    var value = cache.file(trackFile).path(taskHash);
+    assert.equal(typeof value, 'undefined');
   },
 
   'using md5, when the underlying file changes, the stored items are invalidated': function() {
-    // should emit "outdated"
+    var cache = Cache.instance({ path: cachePath, method: 'md5' }),
+        taskHash = 'test3',
+        trackFile = __dirname+'/fixtures/hash2.txt';
+
+    fs.writeFileSync(trackFile, 'foo');
+
+    // create the file in the cache folder
+    cacheFile = cache.filepath();
+    fs.writeFileSync(trackFile, '123');
+    // mark as complete
+    cache.file(trackFile).path(taskHash, cacheFile);
+    assert.equal(cache.file(trackFile).path(taskHash), cacheFile);
+
+    // 1) when the task hash is changed, the lookup is invalidated
+    taskHash = 'test4';
+    assert.ok(!cache.file(trackFile).path(taskHash));
+
+    // 2) when the file changes => invalidate
+    taskHash = 'test3';
+    fs.writeFileSync(trackFile, 'foobar');
+
+    var value = cache.file(trackFile).path(taskHash);
+    assert.equal(typeof value, 'undefined');
   },
 
 /*
-  'can look up a cached item by fs.stat': function() {
-    assert.ok(!cache.lookup(__dirname+'/fixtures/bar.txt', 'simple'));
-
-    // create the file in the cache folder
-    cacheFile = cache.filename();
-    fs.writeFileSync(cacheFile, 'foo');
-    // mark as complete
-    cache.complete(__dirname+'/fixtures/bar.txt', 'simple', cacheFile);
-    assert.ok(cache.lookup(__dirname+'/fixtures/bar.txt', 'simple'));
-  },
-
-  'can look up a cached item by md5': function() {
-    cache = new Cache({
-      path: __dirname+'/cache',
-      method: 'md5'
-    });
-
-    fs.writeFileSync(__dirname+'/fixtures/hash.txt', 'first');
-
-    assert.ok(!cache.lookup(__dirname+'/fixtures/hash.txt', 'test2'));
-    // create the file in the cache folder
-    cacheFile = cache.filename();
-    fs.writeFileSync(cacheFile, 'foo');
-    // mark as complete
-    cache.complete(__dirname+'/fixtures/hash.txt', 'test2', cacheFile);
-    assert.ok(cache.lookup(__dirname+'/fixtures/hash.txt', 'test2'));
-
-    // change the input file => invalidate
-    fs.writeFileSync(__dirname+'/fixtures/hash.txt', 'second');
-    assert.ok(!cache.lookup(__dirname+'/fixtures/hash.txt', 'test2'));
-  },
-
-  'when the execution result does not match, it is not reused': function() {
-    cache = new Cache({
-      path: __dirname+'/cache',
-      method: 'stat'
-    });
-
-    var taskHash = 'test3';
-
-    // create the file in the cache folder
-    cacheFile = cache.filename();
-    fs.writeFileSync(cacheFile, 'foo');
-    // mark as complete
-    cache.complete(__dirname+'/fixtures/hash.txt', taskHash, cacheFile);
-    assert.ok(cache.lookup(__dirname+'/fixtures/hash.txt', taskHash));
-
-    // when the task hash is changed, the lookup is invalidated
-    taskHash = 'test4';
-    assert.ok(!cache.lookup(__dirname+'/fixtures/hash.txt', taskHash));
-  },
-
   'can call Cache.lookup(url)': function() {
 
   }
